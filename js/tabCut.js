5cadfda9416700539ebe0710fd79ba76e2df8860
SanTree <38810588+aishanglly00@users.noreply.github.com>
2018-05-11 08:29:53 +0800

    Add files via upload

---
+++ js/tabCut.js
@@ -0,0 +1,22 @@
+function $(id){
+	return typeof id == "string"?document.getElementById(id):id;
+}
+
+window.onload = function(){
+	var titleName = $("tab-title").getElementsByTagName("li");
+	var tabCount = $("tab-body").getElementsByTagName("ul");
+	if (titleName.length != tabCount.length) {
+		return;
+	}
+	for (var i=0;i <titleName.length;i++) {
+		titleName[i].id = i;
+		titleName[i].onmouseover = function(){
+			for (var j=0;j <titleName.length;j++) {
+				titleName[j].className = "";
+				tabCount[j].style.display = "none";
+			}
+			this.className = "selected";
+			tabCount[this.id].style.display = "block";
+		}
+	}
+}
